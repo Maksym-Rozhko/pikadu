2c151061217f989e93ad8fbf4f02b12f4dcc95e4
Maksym <maksymRozhko7@gmail.com>
2020-11-12 15:34:15 +0200

    feat: login form
    - the authorization form is ready.
    - preparation of posts using js.

--- js/main.js
+++ js/main.js
@@ -6,6 +6,9 @@ menuToggle.addEventListener('click', (e) => {
     menu.classList.toggle('visible');
 });
 
+// const regExpValidEmail = /^\w+@\w\.\w{2,}$/;
+const regExpValidEmail = /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/;
+
 const loginElem = document.querySelector('.login');
 const loginForm = document.querySelector('.login-form');
 const emailInput = document.querySelector('.login-email');
@@ -14,6 +17,13 @@ const loginSignUp = document.querySelector('.login-signup');
 const forgetPassword = document.querySelector('.login-forget');
 const currentUserElem = document.querySelector('.user');
 const userNameElem = document.querySelector('.user-name');
+const exitElem = document.querySelector('.exit');
+const editElem = document.querySelector('.edit');
+const editContainer = document.querySelector('.edit-container');
+const editUserName = document.querySelector('.edit-username');
+const editAvatar = document.querySelector('.edit-avatar');
+const userAvatarElem = document.querySelector('.user-avatar');
+const postsWrapper = document.querySelector('.posts');
 
 const listUsers = [
     {
@@ -35,6 +45,10 @@ const setUsers = {
     LogIn(email, password, handler) {
         // console.log('Log In');
         // console.log(email, password);
+        if(!regExpValidEmail.test(email)) {
+            alert('Email isn\'t valid');
+            return; 
+        }
         const user = this.getUser(email);
         if(user && user.password === password) {
             this.userAuthorization(user);
@@ -43,16 +57,26 @@ const setUsers = {
             alert('The user with this password is not defined.');
         }
     },
-    LogOut() {
-        console.log('Log Out');
+    LogOut(handler) {
+        // console.log('Log Out');
+        this.user = null;
+        handler();
     },
     SignUp(email, password, handler) {
         // console.log('Sign Up');
+        if(!regExpValidEmail.test(email)) {
+            alert('Email isn\'t valid');
+            return; 
+        }
+        if(!email.trim() || !password.trim()) {
+            alert('Email and password fields haven\'t been entered.');
+            return;
+        }
         if(!this.getUser(email)) {
             const user = {
                 email,
                 password,
-                displayName: email
+                displayName: email.substring(0, email.indexOf('@'))
             };
             listUsers.push(user);
             this.userAuthorization(user);
@@ -80,6 +104,49 @@ const setUsers = {
     forgetPass() {
         console.log('Forget password?')
     },
+    editUser(userName, userAvatar, handler) {
+        if(userName) {
+            this.user.displayName = userName;
+        };
+        if(userAvatar) {
+            this.user.avatar = userAvatar;
+        };
+        handler();
+    },
+};
+
+const setPosts = {
+    allPosts: [
+        {
+            title: 'Заголовок поста',
+            text: 'Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Злых, имеет запятых. Маленькая заманивший образ, ipsum продолжил щеке, всеми маленький свою свой великий, если то. Большого семантика осталось текстами. Переписали имени жизни своих меня ты. Мир, щеке буквоград. Все, меня силуэт агентство взгляд инициал свое подзаголовок свой оксмокс власти выйти взобравшись, до текста! Переулка текстов снова образ сбить продолжил. Парадигматическая вопрос выйти ведущими щеке журчит дорогу пунктуация вскоре меня, взгляд точках! Снова составитель заглавных проектах грустный. Собрал сих рыбного lorem последний что текста толку оксмокс наш ты строчка! Взгляд!',
+            tags: [
+                'свежее',
+                'новое',
+                'горячее',
+                'мое',
+                'случайность'
+            ],
+            author: 'maks@gmail.com',
+            date: '11.11.2020, 15:05:44',
+            like: 75,
+            comments: 20,
+        },
+        {
+            title: 'Заголовок поста',
+            text: 'HTML (от англ. HyperText Markup Language — «язык гипертекстовой разметки») — стандартизированный язык разметки веб-страниц во Всемирной паутине. Код HTML интерпретируется браузерами; полученная в результате интерпретации страница отображается на экране монитора компьютера или мобильного устройства.',
+            tags: [
+                'свежее',
+                'новое',
+                'горячее',
+                'случайность'
+            ],
+            author: 'rita@gmail.com',
+            date: '10.10.2020, 12:35:12',
+            like: 35,
+            comments: 17,
+        }
+    ],
 };
 
 const toggleAuthDom = () => {
@@ -89,35 +156,122 @@ const toggleAuthDom = () => {
     if(user) {
         currentUserElem.style.display = '';
         loginElem.style.display = 'none';
-        userNameElem.textContent = user.displayName.split('\@')[0];
+        userNameElem.textContent = user.displayName; //.split('\@')[0];
+        userAvatarElem.src = user.avatar || userAvatarElem.src;
+        // userAvatarElem.src = user.avatar ? user.avatar : userAvatarElem.src;
     } else {
         currentUserElem.style.display = 'none';
         loginElem.style.display = '';
     }
 };
 
-loginForm.addEventListener('submit', e => {
-    e.preventDefault();
+const showAllPosts = () => {
+    let postsHTML = '';
+
+    setPosts.allPosts.forEach(({ title, text, date, author, like, comments, tags }) => {
+
+        postsHTML += `
+            <section class="post">
+                <div class="post-body">
+                    <h2 class="post-title">
+                        ${title}
+                    </h2>
+                    <p class="post-text">
+                        ${text}
+                    <div class="tags">
+                        <a href="#" class="tag">
+                            ${tags}
+                        </a>
+                    </div>
+                </div>
+                <!-- /.post-body -->
+                <div class="post-footer">
+                    <div class="post-buttons">
+                        <button class="post-button likes">
+                            <svg width="19" height="20" class="icon icon-like">
+                                <use xlink:href="images/icons.svg#like"></use>
+                            </svg>
+                            <span class="likes-counter">${like}</span>
+                        </button>
+                        <button class="post-button comments">
+                            <svg width="21" height="21" class="icon icon-comment">
+                                <use xlink:href="images/icons.svg#comments"></use>
+                            </svg>
+                            <span class="comments-counter">${comments}</span>
+                        </button>
+                        <button class="post-button save">
+                            <svg width="20" height="20" class="icon icon-save">
+                                <use xlink:href="images/icons.svg#save"></use>
+                            </svg>
+                        </button>
+                        <button class="post-button share">
+                            <svg width="17" height="19" class="icon icon-share">
+                                <use xlink:href="images/icons.svg#share"></use>
+                            </svg>
+                        </button>
+                    </div>
+                    <!-- /.post-buttons -->
+                    <div class="post-author">
+                        <div class="author-about">
+                            <a href="#" class="author-username">${author.split('\@')[0]}</a>
+                            <span class="post-time">${date}</span>
+                        </div>
+                        <a href="#" class="author-link"><img src="./images/avatar.jpeg" alt="avatar" class="author-avatar"></a>
+                    </div>
+                    <!-- /.post-author -->
+                </div>
+            </section>
+        `;
+    });
+
+    postsWrapper.innerHTML = postsHTML;
+};
+
+const init = () => {
+    loginForm.addEventListener('submit', e => {
+        e.preventDefault();
+        const emailValue = emailInput.value;
+        const passwordValue = passwordInput.value;
     
-    const emailValue = emailInput.value;
-    const passwordValue = passwordInput.value;
+        setUsers.LogIn(emailValue, passwordValue, toggleAuthDom);
+        loginForm.reset();
+        // toggleAuthDom();
+    });
+    
+    loginSignUp.addEventListener('click', e => {
+        e.preventDefault();
+        const emailValue = emailInput.value;
+        const passwordValue = passwordInput.value;
+    
+        setUsers.SignUp(emailValue, passwordValue, toggleAuthDom);
+        loginForm.reset();
+        // toggleAuthDom();
+    });
+    
+    forgetPassword.addEventListener('click', e => {
+        e.preventDefault();
+        setUsers.ForgetPass();
+    });
+    
+    exitElem.addEventListener('click', e => {
+        e.preventDefault();
+        setUsers.LogOut(toggleAuthDom);
+    });
+    
+    editElem.addEventListener('click', e => {
+        e.preventDefault();
+        editContainer.classList.toggle('visible');
+        editUserName.value = setUsers.user.displayName;
+    });
+    
+    editContainer.addEventListener('submit', e => {
+        e.preventDefault();
+        setUsers.editUser(editUserName.value, editAvatar.value, toggleAuthDom);
+        editContainer.classList.remove('visible');
+    });
 
-    setUsers.LogIn(emailValue, passwordValue, toggleAuthDom);
-    // toggleAuthDom();
-});
+    showAllPosts();
+    toggleAuthDom();
+};
 
-loginSignUp.addEventListener('click', e => {
-    e.preventDefault();
-    const emailValue = emailInput.value;
-    const passwordValue = passwordInput.value;
-
-    setUsers.SignUp(emailValue, passwordValue, toggleAuthDom);
-    // toggleAuthDom();
-});
-
-forgetPassword.addEventListener('click', e => {
-    e.preventDefault();
-    setUsers.ForgetPass();
-});
-
-toggleAuthDom();
+document.addEventListener('DOMContentLoaded', init);
